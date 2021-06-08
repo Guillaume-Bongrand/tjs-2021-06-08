e62b98dc2c048f3a995404d5d2e67fe2a060f03e
guillaume B <gui.bgd@gmail.com>
2021-06-08 15:49:39 +0200

    merge prepare 2

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-
+import Button from './components/Button/Button'
 function App() {
   return (
     <div className="App">
-      Hola mundo
+      Hola guapo
+      <hr/>
+      <Button title="Vamos" type="verb"></Button>
+      <Button title="A" type="particule"></Button>
+      <Button type="action"><img src="https://cdn2.iconfinder.com/data/icons/new-year-s-hand-drawn-basic/64/dancer_2-256.png" alt="click"/></Button>
     </div>
+    
   );
 }
 
