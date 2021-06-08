606137a3ef15c6b5f5481d2d9db4e7ba69adde75
guillaume B <gui.bgd@gmail.com>
2021-06-08 16:39:22 +0200

    d1 16h40

--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import './Button.css'
-
+import PropTypes from 'prop-types'
 
 function Button(props) {
+
         let my_color = null;
         switch(props.type){
             case 'verb':
@@ -19,10 +20,21 @@ function Button(props) {
                 break;
         }
 
-     return <button className="Button" style={{backgroundColor: my_color}}>{props.children || props.title}</button>;
+     return <button className="Button clicked" 
+        onClick={(evt) =>{
+            console.log(evt.target.innerText);
+        }}   
+     style={{backgroundColor: my_color}}>{props.children || props.title}</button>;
 
 }
-
+Button.propTypes={
+    children: PropTypes.any,
+    title: PropTypes.string,
+    type: PropTypes.string.isRequired,
+}
+Button.defautltProps = {
+    type: 'default',
+}
 export default Button;
 
 
